ef578f481d2951786134f77131d232d602ab80a7
Matej Puntar <m-puntar@ispace-inc.com>
2021-10-28 17:52:35 +0200

    pre-commit hooks

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dht-exporter
-Prometheus exporter for humidity and temperature sensor data (DHT22, DHT11, AM2302) running on Raspberry pi 
+__Humidity__ and __temperature__ sensor data for (DHT22, DHT11, AM2302) running on Raspberry pi using Adafruit_DHT library.
+
+
+
+## pre-commit Python framework
+Runs black Code Formatter and Flake8 checker to format python code and checks our code’s compliance to PEP8.
+
+To install git hooks in your .git/ directory run:
+`pre-commit install`
+
+To update to the latest versions of hooks run:
+`pre-commit autoupdate`
+
--- dht_to_mqtt.py
+++ dht_to_mqtt.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import argparse
-import random
+
+# import random
 import time
 import Adafruit_DHT
 import paho.mqtt.client as mqtt
@@ -12,28 +13,33 @@ import paho.mqtt.client as mqtt
 # username = 'emqx'
 # password = 'public'
 
+
 def connect_mqtt(broker, port, client_id):
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
             print("Connected to MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            print("Failed to connect, return code {code}".format(code=rc))
 
     client = mqtt.Client(client_id)
-    # client.username_pw_set(username, password)
     client.on_connect = on_connect
     client.connect(broker, port)
     return client
 
+
 def publish_to_mqtt(mqtt_client, topic, value):
     result = mqtt_client.publish(topic, value)
     # result: [0, 1]
-    
+
     status = result[0]
     if status == 0:
         print("Send {value} to topic {topic}".format(value=value, topic=topic))
     else:
-        print("Failed to send message to topic {topic}".format(value=value, format=format))
+        print(
+            "Failed to send message to topic {topic}: {result}".format(
+                topic=topic, result=result
+            )
+        )
 
 
 def get_sensor_data(gpio_pin):
@@ -42,38 +48,84 @@ def get_sensor_data(gpio_pin):
     humidity, temperature = (None, None)
 
     if humid is not None:
-        if abs(humid) < 100:        #If sensor returns veird value ignore it and wait for the next one 
-           humidity = '{0:0.1f}'.format(humid)
+        if (
+            abs(humid) < 100
+        ):  # If sensor returns veird value ignore it and wait for the next one
+            humidity = "{0:0.1f}".format(humid)
 
     if temp is not None:
-        if abs(temp) < 100:        #If sensor returns veird value ignore it and wait for the next one 
-           temperature = '{0:0.1f}'.format(temp)
+        if (
+            abs(temp) < 100
+        ):  # If sensor returns veird value ignore it and wait for the next one
+            temperature = "{0:0.1f}".format(temp)
 
+    print(
+        "humidity: {humidity}, temp: {temp}".format(
+            humidity=humidity, temp=temperature
+        )
+    )
     return (humidity, temperature)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--pull_time", type=int, default=5, help="Pull sensor data every X seconds.")
-    parser.add_argument("-g", "--gpio", type=int, nargs='+', 
-                        help="Set GPIO pin id to listen for DHT sensor data.", 
-                        required=True)
-    parser.add_argument("-r", "--room", type=str, nargs='+', 
-                        help="Set room name.", 
-                        required=True)
-    parser.add_argument("-m", "--mqtt", type=str, default="", help="Send data to MQTT server at this address.")
-    parser.add_argument("-mp", "--mqtt_port", type=int, default=1883, help="MQTT port")
-    parser.add_argument("-t", "--topic_prefix", type=str, default="", help="Send data to this MQTT topic prefix (<prefix>-temp, <prefix>-humid).")
+    parser.add_argument(
+        "-p",
+        "--pull_time",
+        type=int,
+        default=5,
+        help="Pull sensor data every X seconds.",
+    )
+    parser.add_argument(
+        "-g",
+        "--gpio",
+        type=int,
+        nargs="+",
+        help="Set GPIO pin id to listen for DHT sensor data.",
+        required=True,
+    )
+    parser.add_argument(
+        "-r",
+        "--room",
+        type=str,
+        nargs="+",
+        help="Set room name.",
+        required=True,
+    )
+    parser.add_argument(
+        "-m",
+        "--mqtt",
+        type=str,
+        default="",
+        help="Send data to MQTT server at this address.",
+    )
+    parser.add_argument(
+        "-mp", "--mqtt_port", type=int, default=1883, help="MQTT port"
+    )
+    parser.add_argument(
+        "-t",
+        "--topic_prefix",
+        type=str,
+        default="",
+        help="Send data to this MQTT topic prefix (<prefix>-temp, <prefix>-humid).",
+    )
     cli_arguments = parser.parse_args()
 
     if len(cli_arguments.gpio) != len(cli_arguments.room):
-        print("The number of gpio pins set needs to be the same as number of rooms set" \
-              "\n Number of gpio pins: {g}\n Number of rooms: {r}".format(g=len(cli_arguments.gpio), r=len(cli_arguments.room)))
+        print(
+            "The number of gpio pins set needs to be the same as number of rooms set"
+            "\n Number of gpio pins: {g}\n Number of rooms: {r}".format(
+                g=len(cli_arguments.gpio), r=len(cli_arguments.room)
+            )
+        )
         exit(1)
 
-
-    if cli_arguments.topic_prefix!="":
-        mqtt_client = connect_mqtt(cli_arguments.mqtt, cli_arguments.mqtt_port, "-".join((cli_arguments.topic_prefix, cli_arguments.room[0])))
-
+    if cli_arguments.topic_prefix != "":
+        mqtt_client = connect_mqtt(
+            cli_arguments.mqtt,
+            cli_arguments.mqtt_port,
+            "-".join((cli_arguments.topic_prefix, cli_arguments.room[0])),
+        )
 
     # Update temperature and humidity
     while True:
@@ -81,9 +133,29 @@ if __name__ == '__main__':
             print(gpio_pin)
             humidity, temperature = get_sensor_data(gpio_pin)
 
-            if cli_arguments.topic_prefix!="":
-               publish_to_mqtt(mqtt_client, "/".join((cli_arguments.topic_prefix, cli_arguments.room[id], "temperature")), temperature)
-               publish_to_mqtt(mqtt_client, "/".join((cli_arguments.topic_prefix, cli_arguments.room[id], "humidity")), humidity)
+            if cli_arguments.topic_prefix != "":
+                publish_to_mqtt(
+                    mqtt_client,
+                    "/".join(
+                        (
+                            cli_arguments.topic_prefix,
+                            cli_arguments.room[id],
+                            "temperature",
+                        )
+                    ),
+                    temperature,
+                )
+                publish_to_mqtt(
+                    mqtt_client,
+                    "/".join(
+                        (
+                            cli_arguments.topic_prefix,
+                            cli_arguments.room[id],
+                            "humidity",
+                        )
+                    ),
+                    humidity,
+                )
         time.sleep(cli_arguments.pull_time)
 
 
